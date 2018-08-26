fa44b7652eca6b846fd4545bd0f3d0cb2392ed00
Yohan Sebastian Roldan Moreno <sebasroldanm@gmail.com>
2018-08-25 19:48:12 -0500

    COreccion linea 6 -> UContrasenia[Prueba de sincronización]

--- Utilitarios/UContrasenia.cs
+++ Utilitarios/UContrasenia.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-Vamos a provar esto muachos
+
 namespace Utilitarios
 {
     public class UContrasenia
